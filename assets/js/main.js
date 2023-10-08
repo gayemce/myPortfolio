afdd73e78b0f175a462b8865910b5504daff7883
gayemce <100995170+gayemce@users.noreply.github.com>
2023-10-08 22:21:44 +0300

    e-mail sending via the contact form has been activated

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,32 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /* ---------------  EMAIL JS  --------------- */
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_ifaca1','template_fnfhsze','#contact-form','y5SwUO5O0YXGhljWc')
+    .then(() =>{
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        //Remove message after five second
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    }, () =>
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    )
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /* ---------------  SHOW SCROLL UP  --------------- */ 
 
